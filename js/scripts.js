a9173be8788549a4bae9fa60510c3dc3c06a894a
kakaladi <ounveasna@outlook.com>
2025-02-18 11:07:33 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -678,7 +678,7 @@ function createTable() {
         },
         {
             styleName: "line art",
-            imageSrc: "https://kakaladi-catalog.github.io/drawveasna/images/_9JxQYNereSc1w3ZtizGj1Xjb2Yrso1H1ff4cFxcNAQ.jpg",
+            imageSrc: "https://kakaladi-catalog.github.io/drawveasna/images/_9JxQYNereSc1w3ZtizGj1Xjb2Yrso1H1ff4cFxcNAQ.png",
             prompt: "line art drawing, professional, sleek, modern, minimalist, graphic, line art, vector graphics"
         },
         {
